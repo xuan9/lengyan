01f0b9f9a7ce38b5b5a6c5f27f139f6857eb9e04
Fuxuan Gong <fuxuan.gong@nextgentel.com>
2020-10-01 13:39:18 +0200

    fix RATreeView

--- lengyan/Util/RATreeView/Private/RATreeView+Enums.m
+++ lengyan/Util/RATreeView/Private/RATreeView+Enums.m
@@ -98,6 +98,8 @@
       return RATreeViewStylePlain;
     case UITableViewStyleGrouped:
       return RATreeViewStyleGrouped;
+    case UITableViewStyleInsetGrouped:
+      return RATreeViewStyleGrouped;
   }
 }
 #pragma mark Scroll Positions
